e29236dc762f4d81fea6835753e6d6ce3551cd9c
Hemalatha-pichandi <Hemalatha.pichandi@gmail.com>
2014-07-28 19:40:54 +1000

    Updated Tests

--- Mi9CodeChallenge.Tests/Controllers/PayloadControllerTest.cs
+++ Mi9CodeChallenge.Tests/Controllers/PayloadControllerTest.cs
@@ -32,27 +32,27 @@ namespace Mi9CodeChallenge.Tests.Controllers
             Assert.IsInstanceOfType(actionResult, typeof(ErrorResult));
         }
 
-        //[TestMethod]
-        //public void PostReturn400withNullPayload()
-        //{
-        //    IHttpActionResult actionResult = payloadController.Post(new InputPayloadRoot { payload = null });
-        //    //var contentResult = actionResult as NegotiatedContentResult<InputPayloadRoot>;
+        [TestMethod]
+        public void PostReturn400withNullPayload()
+        {
+            IHttpActionResult actionResult = payloadController.Post(new InputPayloadRoot { payload = null });
+            //var contentResult = actionResult as NegotiatedContentResult<InputPayloadRoot>;
 
-        //    Assert.IsInstanceOfType(actionResult, typeof(ErrorResult));
-        //}
+            Assert.IsInstanceOfType(actionResult, typeof(ErrorResult));
+        }
 
-        //[TestMethod]
-        //public void PostReturnContentwithValidPayload()
-        //{
-        //    InputPayloadRoot payload = SetTestData();
-        //    IHttpActionResult actionResult = payloadController.Post(payload);
+        [TestMethod]
+        public void PostReturnContentwithValidPayload()
+        {
+            InputPayloadRoot payload = SetTestData();
+            IHttpActionResult actionResult = payloadController.Post(payload);
 
-        //    StatusCodeResult statusCodeResult = actionResult as StatusCodeResult;
-        //    var negotiatedResult = actionResult as OkNegotiatedContentResult<List<PayloadInfo>>;
-        //    Assert.IsNotNull(negotiatedResult);
-        //    Assert.AreEqual<int>(3, negotiatedResult.Content.Count);
+            StatusCodeResult statusCodeResult = actionResult as StatusCodeResult;
+            var negotiatedResult = actionResult as OkNegotiatedContentResult<List<PayloadInfo>>;
+            Assert.IsNotNull(negotiatedResult);
+            Assert.AreEqual<int>(3, negotiatedResult.Content.Count);
 
-        //}
+        }
         
         private static InputPayloadRoot SetTestData()
         {
@@ -62,19 +62,19 @@ namespace Mi9CodeChallenge.Tests.Controllers
                 new Payload { drm = false, episodeCount = 0},
                 new Payload { drm = false, episodeCount = 1},
                 new Payload { drm = true, episodeCount = 0}, 
-                //new Payload { slug="show/seapatrol", title="Sea Patrol", tvChannel="Channel 9"},               
-                //new Payload { country="UK", description="What's life like when you have enough children to field your own football team?", 
-                //    drm = true, episodeCount = 0, slug = "show/16kidsandcounting", title="16 Kids and Counting", 
-                //              image = new Image
-                //              { 
-                //                  showImage = "http://catchup.ninemsn.com.au/img/jump-in/shows/16KidsandCounting1280.jpg"
-                //              }},
-                //new Payload { country="USA", description="What's life like when you have enough children to field your own football team?", 
-                //    drm = true, episodeCount = 1, slug = "show/16kidsandcounting", title="16 Kids and Counting", 
-                //              image = new Image
-                //              { 
-                //                  showImage = "http://catchup.ninemsn.com.au/img/jump-in/shows/16KidsandCounting1280.jpg"
-                //              }},
+                new Payload { slug="show/seapatrol", title="Sea Patrol", tvChannel="Channel 9"},               
+                new Payload { country="UK", description="What's life like when you have enough children to field your own football team?", 
+                    drm = true, episodeCount = 0, slug = "show/16kidsandcounting", title="16 Kids and Counting", 
+                              image = new Image
+                              { 
+                                  showImage = "http://catchup.ninemsn.com.au/img/jump-in/shows/16KidsandCounting1280.jpg"
+                              }},
+                new Payload { country="USA", description="What's life like when you have enough children to field your own football team?", 
+                    drm = true, episodeCount = 1, slug = "show/16kidsandcounting", title="16 Kids and Counting", 
+                              image = new Image
+                              { 
+                                  showImage = "http://catchup.ninemsn.com.au/img/jump-in/shows/16KidsandCounting1280.jpg"
+                              }},
                 new Payload { slug="show/seapatrol", title="Sea Patrol"},               
                 new Payload { drm = true, episodeCount = 0, slug = "show/16kidsandcounting", title="16 Kids and Counting", 
                               image = new Image
