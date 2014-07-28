4313b5d99016fe4ad03dd4ee98597ce1d3fe1aa6
Hemalatha-pichandi <Hemalatha.pichandi@gmail.com>
2014-07-28 20:23:01 +1000

    Updated JsonProperty Name

--- Mi9CodeChallenge/Models/OutputPayload.cs
+++ Mi9CodeChallenge/Models/OutputPayload.cs
@@ -1,17 +1,25 @@
 ﻿using System.Collections.Generic;
 using System;
+using Newtonsoft.Json;
 
 namespace Mi9CodeChallenge.Models
 {
     public class PayloadInfo
     {
+        [JsonProperty(PropertyName = "image")]
         public string image { get; set; }
+
+        [JsonProperty(PropertyName = "slug")]
         public string slug { get; set; }
+
+        [JsonProperty(PropertyName = "title")]
         public string title { get; set; }
+
     }
 
     public class OutputPayload
     {
+        [JsonProperty(PropertyName = "response")]
         public List<PayloadInfo> payloadInfo { get; set; }
     }
 }
